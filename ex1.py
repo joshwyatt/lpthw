e087d0788721c40f61ca2ec1e660082ad7c1dae7
joshWyatt <joshuawyatt123@gmail.com>
2014-08-29 10:56:41 -0700

    Finish ex1 through ex13

---
+++ ex1.py
@@ -0,0 +1,7 @@
+# print "Hello"
+# print "Hello Again"
+# print "I like typing this."
+# print "This is fun."
+# print "Yay! Printing."
+# print "i'd much rather you 'not'."
+print 'This is some extra credit'
