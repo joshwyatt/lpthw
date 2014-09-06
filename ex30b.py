94f2f9fc921be2c2540d48eab37efe36d78e029d
joshWyatt <joshuawyatt123@gmail.com>
2014-09-06 11:58:21 -0700

    Do ex30b

---
+++ ex30b.py
@@ -0,0 +1,11 @@
+class Person(object):
+
+  def __init__(self, name):
+    self.name = name
+
+  def say_my_name(self):
+    print "My name is %s" % self.name
+
+josh_wyatt = Person('Josh')
+
+josh_wyatt.say_my_name()
